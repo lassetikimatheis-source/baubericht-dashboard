28131663de55cd63249e7550839a5315b72ddb82
Matheis <lasse.matheis@paribus.de>
2026-06-19 10:33:51 +0200

    Add satellite map labels

--- components/map/ObjectMap.tsx
+++ components/map/ObjectMap.tsx
@@ -27,6 +27,18 @@ export function ObjectMap({
         attribution='Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community'
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
       />
+      <TileLayer
+        attribution='Labels &copy; Esri'
+        url="https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}"
+        pane="overlayPane"
+        opacity={0.95}
+      />
+      <TileLayer
+        attribution='Roads &copy; Esri'
+        url="https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/{z}/{y}/{x}"
+        pane="overlayPane"
+        opacity={0.95}
+      />
       <FitBounds entries={entries} />
       {entries.map((entry) => (
         <ObjectMarker key={entry.key} entry={entry} onOpenObject={onOpenObject} />
